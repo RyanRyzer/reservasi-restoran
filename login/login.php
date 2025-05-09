5df426702d8e7ee97ae5ab46c0a3076260e5befb
Ryan Robby <147597256+RyanRyzer@users.noreply.github.com>
2025-05-09 16:53:47 +0700

    Update login.php

--- login/login.php
+++ login/login.php
@@ -6,38 +6,45 @@
 
 <?php
 session_start();
-if($_POST){
-    $username = $_POST['username_608'];
-    $password = $_POST['password_608'];
 
-    if($username == "admin" && $password == "admin123"){
-        $hak_akses = "admin";$valid = true;
-    }else if ($username == "mhs" && $password == "mhs608"){
-        $hak_akses = "mahasiswa";$valid = true;
-    }else{
-        $hak_akses = "";$valid = false;
-        echo "Username atau Password salah";
+class ProsesLogin608 {
+    private $username_608;
+    private $password_608;
+    private $hakAkses_608;
+    private $valid_608 = false;
+
+    public function __construct($username_608, $password_608) {
+        $this->username_608 = $username_608;
+        $this->password_608 = $password_608;
     }
 
-    if($valid){
-        $_SESSION['username'] = $username;
-        $_SESSION['hak_akses'] = $hak_akses;
+public function login608() {
+        if ($this->username_608 === "admin" && $this->password_608 === "admin123") {
+            $this->hakAkses_608 = "admin";
+            $this->valid_608 = true;
+            echo "Selamat datang admin";
+        } elseif ($this->username_608 === "mhs" && $this->password_608 === "mhs608") {
+            $this->hakAkses_608 = "mahasiswa";
+            $this->valid_608 = true;
+            echo "Selamat datang mahasiswa";
+        } else {
+            $this->hakAkses_608 = "";
+            $this->valid_608 = false;
+            echo "Username atau Password salah";
+        }
+
+        if ($this->valid_608) {
+            $_SESSION['username_608'] = $this->username_608;
+            $_SESSION['hak_akses_608'] = $this->hakAkses_608;
+        }
     }
 }
 
-if(!empty($_SESSION['username'])){
-    echo "Nama = ".$_SESSION['username']."<br>";
-    echo "Hak Akses = ".$_SESSION['hak_akses']."<br>";
-}
+if($_POST) {
+    $username_608 = $_POST['username_608'];
+    $password_608 = $_POST['password_608'];
 
-if($_GET){
-    if($_GET['logout'] == "true"){
-        session_unset();
-        session_destroy();
-        echo "Anda sudah logout";
-        header("Location: login.php");
-        exit();
-    }
+    $login608 = new ProsesLogin608($username_608, $password_608);
+    $login608->login608();
 }
-
 ?>
